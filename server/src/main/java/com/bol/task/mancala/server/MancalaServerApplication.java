efe2f6521dc5dcde7510217b714a44e60fd9932c
Mete <bmcnpnr@users.noreply.github.com>
2019-06-15 12:21:13 +0300

    added spring websocket app from spring initalizr

---
+++ server/src/main/java/com/bol/task/mancala/server/MancalaServerApplication.java
@@ -0,0 +1,13 @@
+package com.bol.task.mancala.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MancalaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MancalaServerApplication.class, args);
+	}
+
+}
