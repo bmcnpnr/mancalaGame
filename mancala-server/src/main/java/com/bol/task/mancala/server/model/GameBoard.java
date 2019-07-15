0b205a28268c163b1fcfa35793d47b82cb30fadd
Mete <bmcnpnr@users.noreply.github.com>
2019-07-15 13:05:31 +0300

    handled all cases

--- mancala-server/src/main/java/com/bol/task/mancala/server/model/GameBoard.java
+++ mancala-server/src/main/java/com/bol/task/mancala/server/model/GameBoard.java
@@ -25,8 +25,8 @@ public class GameBoard {
         if (row == 0) {
             if (col != 0) {
                 return playMove(row, col - 1, numOfStones, player);
-            } else if (Player.PLAYER_ONE.equals(player) && numOfStones > 0) {
-                userOneScore++;
+            } else if (Player.PLAYER_TWO.equals(player) && numOfStones > 0) {
+                userTwoScore++;
                 return playMove(row + 1, col, numOfStones - 1, player);
             } else {
                 return playMove(row + 1, col, numOfStones, player);
@@ -34,8 +34,8 @@ public class GameBoard {
         } else if (row == 1) {
             if (col != 5) {
                 return playMove(row, col + 1, numOfStones, player);
-            } else if (Player.PLAYER_TWO.equals(player) && numOfStones > 0) {
-                userTwoScore++;
+            } else if (Player.PLAYER_ONE.equals(player) && numOfStones > 0) {
+                userOneScore++;
                 return playMove(row - 1, col, numOfStones - 1, player);
             } else {
                 return playMove(row - 1, col, numOfStones, player);
@@ -46,54 +46,18 @@ public class GameBoard {
 
     private boolean playMove(int row, int col, int numOfStones, Player player) {
         if (numOfStones == 0) {
-            if (Player.PLAYER_ONE.equals(player) && row == 1 && col == 0) {
+            if (Player.PLAYER_TWO.equals(player) && row == 1 && col == 0) {
                 return true;
             } else {
-                return Player.PLAYER_TWO.equals(player) && row == 0 && col == 5;
+                return Player.PLAYER_ONE.equals(player) && row == 0 && col == 5;
             }
         }
-        if (row == 0 && col >= 0) {
-            if (Player.PLAYER_ONE.equals(player) && col == 0) {
-                if (numOfStones > 2) {
-                    table[row][col] = table[row][col] + 1;
-                    userOneScore++;
-                    return playMove(row + 1, col, numOfStones - 2, player);
-                } else if (numOfStones == 2) {
-                    table[row][col] = table[row][col] + 1;
-                    if (userOneScore == 0) { //"player lands own big pit" case
-                        userOneScore = 1 + userTwoScore;
-                        userTwoScore = 0;
-                    } else {
-                        userOneScore++;
-                    }
-                    return true;
-                } else if (numOfStones == 1) {
-                    if (table[row][col] == 0) { //capture opponent's little pit
-                        table[row][col] = 1 + table[row + 1][col];
-                        table[row + 1][col] = 0;
-                    } else {
-                        table[row][col] = table[row][col] + 1;
-                    }
-                    return false;
-                }
-            } else if (Player.PLAYER_TWO.equals(player) && col == 0) {
-                table[row][col] = table[row][col] + 1;
-                return playMove(row + 1, col, numOfStones - 1, player);
-            } else {
-                if (Player.PLAYER_TWO.equals(player) && table[row][col] == 0 && numOfStones == 1) {
-                    table[row][col] = table[row + 1][col] + 1;
-                    table[row + 1][col] = 0;
-                } else {
-                    table[row][col] = table[row][col] + 1;
-                }
-                return playMove(row, col - 1, numOfStones - 1, player);
-            }
-        } else if (row == 1 && col <= 5) {
-            if (Player.PLAYER_TWO.equals(player) && col == 5) {
+        if (row == 0 && col >= 0) { // only player 2 can capture from little pit
+            if (Player.PLAYER_TWO.equals(player) && col == 0) {
                 if (numOfStones > 2) {
                     table[row][col] = table[row][col] + 1;
                     userTwoScore++;
-                    return playMove(row - 1, col, numOfStones - 2, player);
+                    return playMove(row + 1, col, numOfStones - 2, player);
                 } else if (numOfStones == 2) {
                     table[row][col] = table[row][col] + 1;
                     if (userTwoScore == 0) { //"player lands own big pit" case
@@ -104,20 +68,57 @@ public class GameBoard {
                     }
                     return true;
                 } else if (numOfStones == 1) {
-                    if (table[row][col] == 0) { //capture opponent's little pit
-                        table[row][col] = 1 + table[row - 1][col];
-                        table[row - 1][col] = 0;
+                    if (table[row][col] == 0) {
+                        userTwoScore += table[row + 1][col] + 1;
+                        table[row + 1][col] = 0;
                     } else {
                         table[row][col] = table[row][col] + 1;
                     }
                     return false;
                 }
-            } else if (Player.PLAYER_ONE.equals(player) && col == 5) {
+            } else if (Player.PLAYER_ONE.equals(player) && col == 0) {
+                table[row][col] = table[row][col] + 1;
+                return playMove(row + 1, col, numOfStones - 1, player);
+
+            } else {
+                if (Player.PLAYER_TWO.equals(player) && table[row][col] == 0 && numOfStones == 1) { //capture opponent's little pit
+                    userTwoScore += table[row + 1][col] + 1;
+                    table[row + 1][col] = 0;
+                } else {
+                    table[row][col] = table[row][col] + 1;
+                }
+                return playMove(row, col - 1, numOfStones - 1, player);
+            }
+        } else if (row == 1 && col <= 5) {  // only player 1 can capture from little pit
+            if (Player.PLAYER_ONE.equals(player) && col == 5) {
+                if (numOfStones > 2) {
+                    table[row][col] = table[row][col] + 1;
+                    userOneScore++;
+                    return playMove(row - 1, col, numOfStones - 2, player);
+                } else if (numOfStones == 2) {
+                    table[row][col] = table[row][col] + 1;
+                    if (userOneScore == 0) { //"player lands own big pit" case
+                        userOneScore = 1 + userTwoScore;
+                        userTwoScore = 0;
+                    } else {
+                        userOneScore++;
+                    }
+                    return true;
+                } else if (numOfStones == 1) {
+                    if (table[row][col] == 0) {
+                        userOneScore += 1 + table[row-1][col];
+                        table[row-1][col] = 0;
+                    } else {
+                        table[row][col] = table[row][col] + 1;
+                    }
+                    return false;
+                }
+            } else if (Player.PLAYER_TWO.equals(player) && col == 5) {
                 table[row][col] = table[row][col] + 1;
                 return playMove(row - 1, col, numOfStones - 1, player);
             } else {
                 if (Player.PLAYER_ONE.equals(player) && table[row][col] == 0 && numOfStones == 1) {
-                    table[row][col] = table[row - 1][col] + 1;
+                    userOneScore += table[row - 1][col] + 1;
                     table[row - 1][col] = 0;
                 } else {
                     table[row][col] = table[row][col] + 1;
@@ -132,11 +133,11 @@ public class GameBoard {
         return table;
     }
 
-    public int getUserOneScore() {
+    int getUserOneScore() {
         return userOneScore;
     }
 
-    public int getUserTwoScore() {
+    int getUserTwoScore() {
         return userTwoScore;
     }
 }
--- mancala-webapp/src/MancalaGame.js
+++ mancala-webapp/src/MancalaGame.js
@@ -5,9 +5,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
 import { getStompClient, sendMessage } from './WebSocketManager';
 
 let stompClient;
+let buttonsDisabled = true;
 
 const styles = theme => ({
   row: {
@@ -41,64 +43,64 @@ function GameRows(props) {
     <div className={classes.row}>
       <Grid container spacing={3}>
         <Grid item xs>
-          <Button disabled={props.nextPlayerToPlay === "player1" || props.playerOfThisClient === "player1"}
+          <Button disabled={buttonsDisabled && (props.nextPlayerToPlay === "player1" || props.playerOfThisClient === "player1")}
             onClick={() => props.handlePlayerMove(0, 0)}>{props.gameTable[0][0]}
           </Button>
         </Grid>
         <Grid item xs>
-          <Button disabled={props.nextPlayerToPlay === "player1" || props.playerOfThisClient === "player1"}
+          <Button disabled={buttonsDisabled && (props.nextPlayerToPlay === "player1" || props.playerOfThisClient === "player1")}
             onClick={() => props.handlePlayerMove(0, 1)}>{props.gameTable[0][1]}
           </Button>
         </Grid>
         <Grid item xs>
-          <Button disabled={props.nextPlayerToPlay === "player1" || props.playerOfThisClient === "player1"}
+          <Button disabled={buttonsDisabled && (props.nextPlayerToPlay === "player1" || props.playerOfThisClient === "player1")}
             onClick={() => props.handlePlayerMove(0, 2)}>{props.gameTable[0][2]}
           </Button>
         </Grid>
         <Grid item xs>
-          <Button disabled={props.nextPlayerToPlay === "player1" || props.playerOfThisClient === "player1"}
+          <Button disabled={buttonsDisabled && (props.nextPlayerToPlay === "player1" || props.playerOfThisClient === "player1")}
             onClick={() => props.handlePlayerMove(0, 3)}>{props.gameTable[0][3]}
           </Button>
         </Grid>
         <Grid item xs>
-          <Button disabled={props.nextPlayerToPlay === "player1" || props.playerOfThisClient === "player1"}
+          <Button disabled={buttonsDisabled && (props.nextPlayerToPlay === "player1" || props.playerOfThisClient === "player1")}
             onClick={() => props.handlePlayerMove(0, 4)}>{props.gameTable[0][4]}
           </Button>
         </Grid>
         <Grid item xs>
-          <Button disabled={props.nextPlayerToPlay === "player1" || props.playerOfThisClient === "player1"}
+          <Button disabled={buttonsDisabled && (props.nextPlayerToPlay === "player1" || props.playerOfThisClient === "player1")}
             onClick={() => props.handlePlayerMove(0, 5)}>{props.gameTable[0][5]}
           </Button>
         </Grid>
       </Grid>
       <Grid container spacing={3}>
         <Grid item xs>
-          <Button disabled={props.nextPlayerToPlay === "player2" || props.playerOfThisClient === "player2"}
+          <Button disabled={buttonsDisabled && (props.nextPlayerToPlay === "player2" || props.playerOfThisClient === "player2")}
             onClick={() => props.handlePlayerMove(1, 0)}>{props.gameTable[1][0]}
           </Button>
         </Grid>
         <Grid item xs>
-          <Button disabled={props.nextPlayerToPlay === "player2" || props.playerOfThisClient === "player2"}
+          <Button disabled={buttonsDisabled && (props.nextPlayerToPlay === "player2" || props.playerOfThisClient === "player2")}
             onClick={() => props.handlePlayerMove(1, 1)}>{props.gameTable[1][1]}
           </Button>
         </Grid>
         <Grid item xs>
-          <Button disabled={props.nextPlayerToPlay === "player2" || props.playerOfThisClient === "player2"}
+          <Button disabled={buttonsDisabled && (props.nextPlayerToPlay === "player2" || props.playerOfThisClient === "player2")}
             onClick={() => props.handlePlayerMove(1, 2)}>{props.gameTable[1][2]}
           </Button>
         </Grid>
         <Grid item xs>
-          <Button disabled={props.nextPlayerToPlay === "player2" || props.playerOfThisClient === "player2"}
+          <Button disabled={buttonsDisabled && (props.nextPlayerToPlay === "player2" || props.playerOfThisClient === "player2")}
             onClick={() => props.handlePlayerMove(1, 3)}>{props.gameTable[1][3]}
           </Button>
         </Grid>
         <Grid item xs>
-          <Button disabled={props.nextPlayerToPlay === "player2" || props.playerOfThisClient === "player2"}
+          <Button disabled={buttonsDisabled && (props.nextPlayerToPlay === "player2" || props.playerOfThisClient === "player2")}
             onClick={() => props.handlePlayerMove(1, 4)}>{props.gameTable[1][4]}
           </Button>
         </Grid>
         <Grid item xs>
-          <Button disabled={props.nextPlayerToPlay === "player2" || props.playerOfThisClient === "player2"}
+          <Button disabled={buttonsDisabled && (props.nextPlayerToPlay === "player2" || props.playerOfThisClient === "player2")}
             onClick={() => props.handlePlayerMove(1, 5)}>{props.gameTable[1][5]}
           </Button>
         </Grid>
@@ -115,7 +117,8 @@ class MancalaGame extends React.Component {
       playerOfThisClient: null,
       nextPlayerToPlay: null,
       playerOneScore: 0,
-      playerTwoScore: 0
+      playerTwoScore: 0,
+      winnerMessage: ""
     };
     this.handlePlayerMove = this.handlePlayerMove.bind(this);
 
@@ -129,15 +132,20 @@ class MancalaGame extends React.Component {
     stompClient.connect({}, function (frame) {
       stompClient.subscribe('/topic/mancala-notifications', function (message) {
         const jsonObj = JSON.parse(message.body);
-        console.log(message.body);
-
-        this.setState({
-          gameTable: jsonObj.table,
-          playerOfThisClient: this.state.playerOfThisClient == null ? jsonObj.playerOfThisClient : this.state.playerOfThisClient,
-          nextPlayerToPlay: jsonObj.nextPlayerToPlay,
-          playerOneScore: jsonObj.userOneScore,
-          playerTwoScore: jsonObj.userTwoScore
-        });
+        if (jsonObj.gameFinished !== undefined) {
+          buttonsDisabled = true;
+          this.setState({
+            winnerMessage: jsonObj.gameFinished
+          });
+        } else {
+          this.setState({
+            gameTable: jsonObj.table,
+            playerOfThisClient: this.state.playerOfThisClient == null ? jsonObj.playerOfThisClient : this.state.playerOfThisClient,
+            nextPlayerToPlay: jsonObj.nextPlayerToPlay,
+            playerOneScore: jsonObj.userOneScore,
+            playerTwoScore: jsonObj.userTwoScore
+          });
+        }
       }.bind(this));
       stompClient.send("/mancalaGame/connectToGame");
     }.bind(this));
@@ -148,7 +156,7 @@ class MancalaGame extends React.Component {
       <div className={classes.root}>
         <Grid container spacing={3} direction={'row'}>
           <Grid item xs>
-            <Paper className={classes.paper}>{this.state.playerOneScore}</Paper>
+            <Paper className={classes.paper}>{"Player 2: " + this.state.playerTwoScore}</Paper>
           </Grid>
           <Grid item xs>
             <GameRows handlePlayerMove={this.handlePlayerMove}
@@ -157,9 +165,12 @@ class MancalaGame extends React.Component {
               playerOfThisClient={this.state.playerOfThisClient} />
           </Grid>
           <Grid item xs>
-            <Paper className={classes.paper}>{this.state.playerTwoScore}</Paper>
+            <Paper className={classes.paper}>{"Player 1: " + this.state.playerOneScore}</Paper>
           </Grid>
         </Grid>
+        <Box textAlign="center" m={1}>
+        {this.state.winnerMessage}
+      </Box>
       </div>
     );
   }
